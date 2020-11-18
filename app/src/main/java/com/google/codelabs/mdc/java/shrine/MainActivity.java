7682bb5a83a6f0eaec95bdd12701306c7c12f52d
Adit <aditshah2002@gmail.com>
2020-11-18 22:00:56 +0530

    feat: All set for adding Payment Gateway !

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.google.codelabs.mdc.java.shrine;
 
+import android.app.Activity;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,11 +14,19 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultListener;
+import com.razorpay.PaymentResultWithDataListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements NavigationHost {
+public class MainActivity extends AppCompatActivity implements NavigationHost{
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,29 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
                     .commit();
         }
 
+        // Snippet to integrate with RazorPay
+        /* Checkout.preload(getApplicationContext());
+        Checkout checkout = new Checkout();
+        checkout.setImage(R.drawable.shr_logo);
+        checkout.setKeyID("rzp_test_KUqhcKfJBlqIsq");
+        final Activity activity = this;
+        try {
+            JSONObject options = new JSONObject();
+
+            options.put("name", "Merchant Name");
+            options.put("description", "Reference No. #123456");
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount", "50000");//pass amount in currency subunits
+            options.put("prefill.email", "gaurav.kumar@example.com");
+            options.put("prefill.contact","9988776655");
+            checkout.open(activity, options);
+        } catch(Exception e) {
+            Log.e(TAG, "Error in starting Razorpay Checkout", e);
+        }*/
+        
         //Snippet to store data in Cloud Firestore
         /* DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("Sampledata").document("Inspiration");
         Map<String, Object> dataTosave = new HashMap<String, Object>();
