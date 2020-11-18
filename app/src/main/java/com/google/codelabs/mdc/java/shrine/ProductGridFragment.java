146e5bbc749522687107db1a6e9d026836b93b97
Dishant Vyas <keenlearner13579@gmail.com>
2020-11-18 15:54:47 +0530

    Changed CANCEL button to RESET

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -1,6 +1,5 @@
 package com.google.codelabs.mdc.java.shrine;
 
-import android.os.Build;
 import android.os.Bundle;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,16 +8,11 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
-
 import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-
-
-import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
-import com.google.codelabs.mdc.java.shrine.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter;
 
 public class ProductGridFragment extends Fragment {
 
